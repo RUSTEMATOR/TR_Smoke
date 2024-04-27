dad6810c2279381da3b4e4f468a7933eed8b1dfd
Rustem <samoilenkorustem@gmail.com>
2024-04-27 15:18:01 +0300

    configured ci/cd

--- test_tr_smoke.py
+++ test_tr_smoke.py
@@ -15,83 +15,83 @@ from Suites.Users_profile.users_profile import PersonalInfVer
 from Suites.Users_profile.security import Security
 from Suites.Users_profile.wallet import PositiveWalletTest
 
-# @allure.suite("Registration")
-# def test_registration(page):
-#     registration = Registration(page)
-#     registration.open_site()
-#
-#
-# @allure.suite("Negative registration")
-# @pytest.mark.parametrize("email", [(e) for e in TestData.test_data])
-# def test_negativereg(page, email):
-#     negative_reg = NegativeReg(page)
-#     negative_reg.test_negative_registration(page, email)
-#
-# @allure.suite("Login_Signup via registration form")
-# def test_login(page):
-#     login = LoginRegistration(page)
-#     login.open_site()
-#
-# @allure.suite("Login_Signup via login button")
-# def test_login_button(page):
-#   login = LoginButton(page)
-#   login.open_site()
-#
-# @allure.suite("Logout")
-# def test_logout(page):
-#     logout = Logout(page)
-#     logout.open_site()
-#
-# @allure.suite("Sidebar")
-# def test_casino_games_side(page):
-#     casino_games_side = CasinoGamesSide(page)
-#     casino_games_side.open_site()
-#     allure.attach(page.screenshot(), name='Casino games side opened', attachment_type=allure.attachment_type.PNG)
-#
-# @allure.suite("Sidebar")
-# def test_casino_side_tabs(page):
-#     casino_side_tabs = CasinoSideTabs(page)
-#     casino_side_tabs.open_site()
-#     allure.attach(page.screenshot(), name='Casino side tabs opened', attachment_type=allure.attachment_type.PNG)
-#
-#
-# @allure.suite("Banners test")
-# def test_banners(page):
-#     banners = BannersZeroDep(page)
-#     banners.set_up()
-#     banners.check_banners_position()
-#
-#
-# @allure.suite("Games suite")
-# def test_search_input(page):
-#     search = InputTest(page)
-#     search.set_up()
-#     search.click_on_input_field()
-#
-# @allure.suite("Games suite")
-# def test_demo_mode(page):
-#     search = DemoTest(page)
-#     search.set_up()
-#     search.click_on_input_field()
+@allure.suite("Registration")
+def test_registration(page):
+    registration = Registration(page)
+    registration.open_site()
 
-# @allure.suite("Game suite")
-# def test_providers_dropdown(page):
-#     providers = ProvidersTest(page)
-#     providers.set_up()
-#     providers.open_provider_dropdown()
 
-# @allure.suite("Users profile suite")
-# def test_personal_info_verification(page):
-#     personal_info_verification = PersonalInfVer(page)
-#     personal_info_verification.set_up()
-#     personal_info_verification.enter_user_account()
-#
-#
-# @allure.suite("Users profile suite")
-# def test_security_tab(page):
-#     security_tab = Security(page)
-#     security_tab.set_up()
-#     security_tab.enter_user_account()
+@allure.suite("Negative registration")
+@pytest.mark.parametrize("email", [(e) for e in TestData.test_data])
+def test_negativereg(page, email):
+    negative_reg = NegativeReg(page)
+    negative_reg.test_negative_registration(page, email)
+
+@allure.suite("Login_Signup via registration form")
+def test_login(page):
+    login = LoginRegistration(page)
+    login.open_site()
+
+@allure.suite("Login_Signup via login button")
+def test_login_button(page):
+  login = LoginButton(page)
+  login.open_site()
+
+@allure.suite("Logout")
+def test_logout(page):
+    logout = Logout(page)
+    logout.open_site()
+
+@allure.suite("Sidebar")
+def test_casino_games_side(page):
+    casino_games_side = CasinoGamesSide(page)
+    casino_games_side.open_site()
+    allure.attach(page.screenshot(), name='Casino games side opened', attachment_type=allure.attachment_type.PNG)
+
+@allure.suite("Sidebar")
+def test_casino_side_tabs(page):
+    casino_side_tabs = CasinoSideTabs(page)
+    casino_side_tabs.open_site()
+    allure.attach(page.screenshot(), name='Casino side tabs opened', attachment_type=allure.attachment_type.PNG)
+
+
+@allure.suite("Banners test")
+def test_banners(page):
+    banners = BannersZeroDep(page)
+    banners.set_up()
+    banners.check_banners_position()
+
+
+@allure.suite("Games suite")
+def test_search_input(page):
+    search = InputTest(page)
+    search.set_up()
+    search.click_on_input_field()
+
+@allure.suite("Games suite")
+def test_demo_mode(page):
+    search = DemoTest(page)
+    search.set_up()
+    search.click_on_input_field()
+
+@allure.suite("Game suite")
+def test_providers_dropdown(page):
+    providers = ProvidersTest(page)
+    providers.set_up()
+    providers.open_provider_dropdown()
+
+@allure.suite("Users profile suite")
+def test_personal_info_verification(page):
+    personal_info_verification = PersonalInfVer(page)
+    personal_info_verification.set_up()
+    personal_info_verification.enter_user_account()
+
+
+@allure.suite("Users profile suite")
+def test_security_tab(page):
+    security_tab = Security(page)
+    security_tab.set_up()
+    security_tab.enter_user_account()
 
 @allure.suite("Users profile suite")
 def test_wallet_positive(page):
