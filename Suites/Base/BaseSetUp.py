9a6aafbf3989563e53521ebb2f637eab612516e9
RUSTEMATOR <samoilenkorustem@gmail.com>
2024-08-27 17:27:06 +0300

    fixes made

--- Suites/Base/BaseSetUp.py
+++ Suites/Base/BaseSetUp.py
@@ -8,12 +8,12 @@ from Suites.Locators.page_elements import PageElementsGames
 class BaseSetUp(PageElementsGames):
 
     def __init__(self, playwright: Playwright):
-        self.browser = playwright.chromium.launch(headless=False)
-                                                  # proxy={
-                                                  #     'server': 'http://138.197.150.103:8090',
-                                                  #     'username': 'kbc',
-                                                  #     'password': '347SP&Uwqt!2xZ7w',
-                                                  # }
+        self.browser = playwright.chromium.launch(headless=False,
+                                                  proxy={
+                                                      'server': 'http://138.197.150.103:8090',
+                                                      'username': 'kbc',
+                                                      'password': '347SP&Uwqt!2xZ7w',
+                                                  })
 
         self.context = self.browser.new_context()
         self.page = self.context.new_page()
